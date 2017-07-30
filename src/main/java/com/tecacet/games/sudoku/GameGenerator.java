80f36a9db5bd0027682ac57429a1a4f4a600b0cc
dimitri <dimitri@trizic.com>
2017-07-29 22:00:19 -0700

    formatting

--- src/main/java/com/tecacet/games/sudoku/GameGenerator.java
+++ src/main/java/com/tecacet/games/sudoku/GameGenerator.java
@@ -5,65 +5,53 @@ import java.util.LinkedList;
 //TODO generate valid Sudoku games
 public class GameGenerator {
 
-    private static final int[] VALUES = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    private Sudoku game = new Sudoku();
-    //private Random generator = new Random();
+	private static final int[] VALUES = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	private Sudoku game = new Sudoku();
 
-    public void generateSeedGame() {
-        LinkedList<Integer> values = new LinkedList<Integer>();
-        for (int i : VALUES) {
-            values.add(i);
-        }
-        int size = game.getSize();
+	public void generateSeedGame() {
+		LinkedList<Integer> values = new LinkedList<Integer>();
+		for (int i : VALUES) {
+			values.add(i);
+		}
+		int size = game.getSize();
 
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < size; col++) {
-                game.setValue(row, col, values.get(col));
-            }
-            rotateleft(values,3);
-        }
-        rotateleft(values,1);
-        for (int row = 3; row < 6; row++) {
-            for (int col = 0; col < size; col++) {
-                game.setValue(row, col, values.get(col));
-            }
-            rotateleft(values,3);
-        }
-        rotateleft(values,1);
-        for (int row = 6; row < 9; row++) {
-            for (int col = 0; col < size; col++) {
-                game.setValue(row, col, values.get(col));
-            }
-            rotateleft(values,3);
-        }
-    }
+		for (int row = 0; row < 3; row++) {
+			for (int col = 0; col < size; col++) {
+				game.setValue(row, col, values.get(col));
+			}
+			rotateleft(values, 3);
+		}
+		rotateleft(values, 1);
+		for (int row = 3; row < 6; row++) {
+			for (int col = 0; col < size; col++) {
+				game.setValue(row, col, values.get(col));
+			}
+			rotateleft(values, 3);
+		}
+		rotateleft(values, 1);
+		for (int row = 6; row < 9; row++) {
+			for (int col = 0; col < size; col++) {
+				game.setValue(row, col, values.get(col));
+			}
+			rotateleft(values, 3);
+		}
+	}
 
-    private void rotateleft(LinkedList<?> list, int times) {
-        for (int i = 0; i < times; i++) {
-            rotateleft(list);
-        }
-    }
+	private void rotateleft(LinkedList<?> list, int times) {
+		for (int i = 0; i < times; i++) {
+			rotateleft(list);
+		}
+	}
 
-    private <T> void rotateleft(LinkedList<T> list) {
-        list.add(list.removeFirst());
-    }
+	private <T> void rotateleft(LinkedList<T> list) {
+		list.add(list.removeFirst());
+	}
 
-//    private void setValidValue(int i, int j, Random r) throws UnsolvableGameException {
-//        for (int tries = 0; tries < 10; tries++) {
-//            int value = r.nextInt(game.getSize()) + 1;
-//            if (game.isValidValue(i, j, value)) {
-//                game.setValue(i, j, value);
-//                return;
-//            }
-//        }
-//        throw new UnsolvableGameException();
-//    }
+	public static void main(String[] args) {
+		GameGenerator generator = new GameGenerator();
+		generator.generateSeedGame();
+		System.out.println(generator.game);
 
-    public static void main(String[] args) {
-        GameGenerator generator = new GameGenerator();
-        generator.generateSeedGame();
-        System.out.println(generator.game);
-
-    }
+	}
 
 }
--- src/main/java/com/tecacet/games/sudoku/gui/action/LoadGameAction.java
+++ src/main/java/com/tecacet/games/sudoku/gui/action/LoadGameAction.java
@@ -7,7 +7,6 @@ import java.io.File;
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
 
-
 import com.tecacet.games.sudoku.gui.SudokuController;
 import com.tecacet.games.sudoku.io.FileSystemGameReader;
 import com.tecacet.games.sudoku.io.GameReader;
@@ -15,42 +14,32 @@ import com.tecacet.games.sudoku.io.GameReader;
 @SuppressWarnings("serial")
 public class LoadGameAction extends AbstractAction {
 
-    private JFileChooser fc;
+	private JFileChooser fc;
 
-    private SudokuController controller;
-    private Component parent;
-    private GameReader reader;
-    
-    public LoadGameAction(SudokuController controller, Component parent) {
-        super("Open Game");
-        this.controller = controller;
-        this.parent = parent;
-        reader = new FileSystemGameReader();
-        try {
-            fc = new JFileChooser();
-            fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        } catch (SecurityException se) {
-            //applets in general do not allow filesystem access
-            this.setEnabled(false);
-        }
-        
-        
-        
-        
-//        try {
-//            fc.setCurrentDirectory(new File("./games"));
-//        } catch (Exception e) {
-//            //probably a security exception
-//        }
-        //TODO fc.setFileFilter(filter)
-    }
+	private SudokuController controller;
+	private Component parent;
+	private GameReader reader;
 
-    public void actionPerformed(ActionEvent e) {
-        int returnVal = fc.showOpenDialog(parent);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            File file = fc.getSelectedFile();
-            controller.loadGame(file.getAbsolutePath(),reader);
-        }
-    }
+	public LoadGameAction(SudokuController controller, Component parent) {
+		super("Open Game");
+		this.controller = controller;
+		this.parent = parent;
+		reader = new FileSystemGameReader();
+		try {
+			fc = new JFileChooser();
+			fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		} catch (SecurityException se) {
+			// applets in general do not allow filesystem access
+			this.setEnabled(false);
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		int returnVal = fc.showOpenDialog(parent);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = fc.getSelectedFile();
+			controller.loadGame(file.getAbsolutePath(), reader);
+		}
+	}
 
 }
